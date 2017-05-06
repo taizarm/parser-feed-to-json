d76306d9ea1d195d5694468b1d1b3833a3e05d06
Taiza <taizarabello@gmail.com>
2017-05-06 17:08:54 -0300

    Adding initial version of parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,10 +1,44 @@
 import requests
+from xml.etree import ElementTree
 
 
 class Parser(object):
 
     def __init__(self):
         self.url = 'http://revistaautoesporte.globo.com/rss/ultimas/feed.xml'
+        self.xml_root = ''
+        self.json = {'feed': []}
 
-    def get_feed_url_content(self):
-        return requests.get(self.url)
+    def populate_xml_content(self):
+        response = requests.get(self.url)
+        self.xml_root = ElementTree.fromstring(response.content)
+
+    def return_items_elements(self):
+        return self.xml_root.findall('./channel/item')
+
+    def parse_string_element(self, item, item_name):
+        return item.find(item_name).text
+
+    def parse_description_element(self, item):
+        description = []
+
+        desc_content = self.parse_string_element(item, 'description')
+
+        return description
+
+    def parse_item(self, item):
+        item = {
+            'title': self.parse_string_element(item, 'title'),
+            'link': self.parse_string_element(item, 'link'),
+            'description': self.parse_description_element(item)
+        }
+        return item
+
+    def parse_xml(self):
+        self.populate_xml_content()
+
+        items = self.return_items_elements()
+
+        for item in items:
+            self.json['feed'].append(self.parse_item(item))
+
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,15 +1,76 @@
-import unittest
-import requests
+import pytest
+from xml.etree import ElementTree
 from parser.parser import Parser
 
 
-class TestParser(unittest.TestCase):
+class TestParser():
 
-    def test_get_feed_url_content(self):
-        parser = Parser()
-        response = parser.get_feed_url_content()
-        self.assertEqual(response.status_code, requests.codes.ok)
-        self.assertEqual(response.headers.get('Content-Type'), 'text/xml; charset=utf-8')
+    test_scenarios_items_elements = [
+        ('<rss><channel></channel></rss>', 0),
+        ('<rss><channel> <item></item> <item></item> </channel></rss>', 2)
+    ]
 
-if __name__ == '__main__':
-    unittest.main()
+    def setup_method(self):
+        self.parser = Parser()
+
+    def test_init(self):
+        assert self.parser.url is not None
+        assert self.parser.json is not None
+        assert self.parser.json['feed'] is not None
+
+    def test_populate_xml_content(self):
+        self.parser.populate_xml_content()
+        assert len(self.parser.xml_root) > 0
+
+    @pytest.mark.parametrize('mock_xml, items_number',
+                             test_scenarios_items_elements)
+    def test_return_items_elements(self, mock_xml, items_number):
+        self.parser.xml_root = ElementTree.fromstring(mock_xml)
+
+        res = self.parser.return_items_elements()
+        assert len(res) == items_number
+
+    def test_parse_string_element(self):
+        mock_xml = ('<rss><channel> '
+                    '   <item><tag_name>Content of tag</tag_name></item> '
+                    '</channel></rss>')
+        self.parser.xml_root = ElementTree.fromstring(mock_xml)
+
+        items = self.parser.return_items_elements()
+        res = self.parser.parse_string_element(items[0], 'tag_name')
+        assert res == 'Content of tag'
+
+    def test_parse_description_element(self):
+        mock_xml = ('<rss><channel> '
+                    '   <item>'
+                    '       <description></description>'
+                    '   </item> '
+                    '</channel></rss>')
+        self.parser.xml_root = ElementTree.fromstring(mock_xml)
+
+        items = self.parser.return_items_elements()
+        res = self.parser.parse_description_element(items[0])
+        assert res == []
+
+    def test_parse_item(self):
+        mock_xml = ('<rss><channel> <item>'
+                    '   <title>Title</title>'
+                    '   <link>URL Link</link>'
+                    '   <description>Content of description</description>'
+                    '</item> </channel></rss>')
+        self.parser.xml_root = ElementTree.fromstring(mock_xml)
+
+        items = self.parser.return_items_elements()
+        res = self.parser.parse_item(items[0])
+        assert res['title'] == 'Title'
+        assert res['link'] == 'URL Link'
+        assert res['description'] == []
+
+    def test_parse_xml(self):
+        mock_xml = ('<rss><channel> '
+                    '   <item><tag_name>Content of tag</tag_name></item> '
+                    '</channel></rss>')
+        self.parser.xml_root = ElementTree.fromstring(mock_xml)
+
+        self.parser.parse_xml()
+        assert 'feed' in self.parser.json
